ea6d32c5f890afa81a677dd5a77a6b756b9915c1
nicosaade <jorge.saade@keyinstitute.edu.sv>
2025-06-02 12:21:34 -0600

    hola

--- Calculo de propina.py
+++
@@ -1,4 +0,0 @@
-total = float(input("total de la cuenta: "))
-propina = float(input("porcentaje de propina: "))
-totalmaspropina = total + (total / propina)
-print("El total de la cuenta mas propina es: ", totalmaspropina)
--- Generador del correo key.py
+++
@@ -1,6 +0,0 @@
-NombreCompleto = str(input("Ingrese su nombre completo: "))
-partes = NombreCompleto.strip().split()
-PrimerNombre = partes[0].lower()
-PrimerApellido = partes[2].lower()
-Correo = PrimerNombre + "." + PrimerApellido + "@keyinstitute.edu.sv"
-print("El correo que se debe asignar al usuario ingresado es:",Correo)
